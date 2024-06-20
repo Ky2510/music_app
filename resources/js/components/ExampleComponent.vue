324d399ade19b93ea54f14019e80e0765fa867c8
Ky2510 <rinaldirizqifauzi@gmail.com>
2024-06-20 17:20:39 +0700

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>List of Albums</h2>
+        <ul>
+            <li v-for="album in albums" :key="album.id">{{ album.name }}</li>
+        </ul>
+    </div>
+</template>
